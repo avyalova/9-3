f3416e700a8c9a33ae891b217c24cb5b33fd8dce
Anna <anna_vyalova@yahoo.com>
2023-02-02 23:37:03 +0100

    Task 9-3

---
+++ 01/intro.js
@@ -0,0 +1,15 @@
+function expect(value) {
+  return {
+    toBe: (exp) => {
+      if (value == exp) {
+        console.log("Sucess")
+      } else {
+        console.error(`Value is ${value},but expectation is ${exp}`)
+      }
+    },
+  }
+}
+
+const sum = (a, b) => a + b
+
+module.exports = { sum }
---
+++ 01/intro.test.js
@@ -0,0 +1,19 @@
+const { sum } = require("./intro")
+
+describe("Sum function", () => {
+  test("Sum should return sum of two values", () => {
+    expect(sum(1, 5)).toBe(6)
+    expect(sum(1, 3)).toBe(4)
+  })
+
+  test("Sum should return value correctly comparing to other", () => {
+    expect(sum(1, 5)).toBeGreaterThan(5)
+    expect(sum(1, 3)).toBeGreaterThanOrEqual(4)
+    expect(sum(1, 5)).toBeLessThan(10)
+    expect(sum(1, 3)).toBeLessThanOrEqual(5)
+  })
+
+  test("Sum should  sum 2 float values", () => {
+    expect(sum(0.1, 0.5)).toBe(0.8)
+  })
+})
