04411e655e01e5f0bc4e0cad987f2f9348d5ecc5
Anna <anna_vyalova@yahoo.com>
2023-02-13 22:37:08 +0100

    Modify

--- 01/students.js
+++ 01/students.js
@@ -1,27 +1,55 @@
-function theBest(studentsArray) {
+const studentsArray = [
+  [
+    { name: "Ivan", score: 35, date: "2022-10-11" },
+    { name: "Maria", score: 5, date: "2022-10-10" },
+    { name: "Olga", score: 0, date: "" },
+    { name: "Stepan", score: 35, date: "2022-10-12" },
+    { name: "Oleg", score: 15, date: "2022-10-01" },
+    { name: "Zanna", score: 15, date: "2022-10-11" },
+  ],
+  [
+    { name: "Margo", score: 0, date: "2022-10-11" },
+    { name: "Natalia", score: 25, date: "2022-10-10" },
+    { name: "Marina", score: 25, date: "2022-10-01" },
+    { name: "Dmitry", score: 25, date: "2022-10-12" },
+    { name: "Denis", score: 0, date: "2022-10-02" },
+    { name: "Vadimyr", score: 1, date: "2022-10-11" },
+  ],
+  [
+    { name: "Irina", score: 0, date: "2022-10-11" },
+    { name: "Vasily", score: 0, date: "2022-10-10" },
+    { name: "David", score: 0, date: "2022-10-11" },
+    { name: "Kristina", score: 0, date: "2022-10-12" },
+    { name: "Varvara", score: 0, date: "2022-10-01" },
+    { name: "Tanya", score: 0, date: "2022-10-11" },
+  ],
+]
+
+function theBest(students) {
   let max = 0
-  let newArray = []
-  let index1
-  let index2
-  studentsArray.forEach((element, index) => {
-    for (let i = 0; i < element.length; i++) {
-      if (element[i].score > max) {
-        max = element[i].score
-        index1 = index
-        index2 = i
-      } else if (
-        element[i].score === max &&
-        element[i].date < studentsArray[index1][index2].date
-      ) {
-        max = element[i].score
-        index1 = index
-        index2 = i
+  let bestDate = new Date(0)
+  bestStudentsNames = []
+
+  students.forEach((element) => {
+    console.log("Elemnt score is - " + element.score)
+    if (element.score > max) {
+      max = element.score
+      bestDate = new Date(element.date)
+      bestStudentsNames = []
+      bestStudentsNames.push(element.name)
+      console.log(bestStudentsNames)
+    } else if (element.score == max) {
+      if (new Date(element.date) < bestDate) {
+        bestDate = new Date(element.date)
+        bestStudentsNames = []
+        bestStudentsNames.push(element.name)
+        console.log(bestStudentsNames)
       }
     }
   })
-
-  console.log(`Best student ${studentsArray[index1][index2].name}`)
-  return `Best student ${studentsArray[index1][index2].name}`
+  console.log(bestStudentsNames)
+  return bestStudentsNames
 }
 
+//theBest(studentsArray[0])
 module.exports = theBest
--- 01/students.test.js
+++ 01/students.test.js
@@ -1,19 +1,12 @@
 const theBest = require("./students")
-let studentsArray = []
 
-describe("Test to found the best student", () => {
-  it("Score =  35", () => {
-    expect(theBest(studentsArray)).toBe("Best student Ivan")
-  })
-})
-
-studentsArray = [
+let studentsArray = [
   [
     { name: "Ivan", score: 35, date: "2022-10-11" },
     { name: "Maria", score: 5, date: "2022-10-10" },
     { name: "Olga", score: 0, date: "" },
     { name: "Stepan", score: 35, date: "2022-10-12" },
-    { name: "Oleg", score: 9, date: "2022-10-01" },
+    { name: "Oleg", score: 15, date: "2022-10-01" },
     { name: "Zanna", score: 15, date: "2022-10-11" },
   ],
   [
@@ -33,3 +26,19 @@ studentsArray = [
     { name: "Tanya", score: 0, date: "2022-10-11" },
   ],
 ]
+
+// describe("Test to found the best student", () => {
+//   it("Score =  35", () => {
+//     expect(theBest(studentsArray)).toBe("Best student Ivan")
+//   })
+// })
+describe("Check the groups", () => {
+  it.each([
+    [studentsArray[0], ["Ivan"]],
+    [studentsArray[1], ["Marina"]],
+    [studentsArray[2], []],
+  ])("The best result in group is", (a, result) => {
+    //console.log(studentsArray[0])
+    expect(theBest(a)).toEqual(result)
+  })
+})
